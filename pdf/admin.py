4684b6c0d06479e9bb641ce56211da58669c4130
was07 <cacingcau456@gmail.com>
2018-02-07 15:14:38 +0700

    inline 2 level with grappelli

--- pdf/admin.py
+++ pdf/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
+from django.forms import TextInput, Textarea
+from django.db import models
 
 from pdf.models import *
 from pdf.list_filter import *
 # Register your models here.
+class CoreDescInline(admin.TabularInline):
+    model = CoreDesc
+    extra = 0
+    classes = ('grp-collapse grp-open',)
+#    show_change_link = True
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 150})},
+    }
+
+class FrameworkInline(admin.TabularInline):
+    model = Framework
+    extra = 0
+    classes = ('grp-collapse grp-open',)
+#    show_change_link = True
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 150})},
+    }
+
 class CoreAdmin(admin.ModelAdmin):
     list_display = ['name']
     list_filter = ()
     search_fields = ['name']
     list_per_page = 25
+    inlines = [CoreDescInline, FrameworkInline]
 
 admin.site.register(Core, CoreAdmin)
 
@@ -19,11 +40,30 @@ class StageAdmin(admin.ModelAdmin):
 
 admin.site.register(Stage, StageAdmin)
 
+class FrameDescInline(admin.TabularInline):
+    model = FrameworkDesc
+    extra = 0
+    show_change_link = True
+    classes = ('grp-collapse grp-open',)
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 150})},
+    }
+
+class IndicatorInline(admin.TabularInline):
+    model = Indicator
+    extra = 0
+    show_change_link = True
+    classes = ('grp-collapse grp-open',)
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 1, 'cols': 150})},
+    }
+
 class FrameworkAdmin(admin.ModelAdmin):
     list_display = ['name', 'core']
     list_filter = ('core',)
     search_fields = ['name']
     list_per_page = 25
+    inlines = [FrameDescInline, IndicatorInline,]
 
 admin.site.register(Framework, FrameworkAdmin)
 
@@ -32,6 +72,8 @@ class IndicatorAdmin(admin.ModelAdmin):
     list_filter = ('framework__core', FrameworkListFilter, 'stage',)
     search_fields = ['name']
     list_per_page = 25
+    change_list_filter_template = "admin/filter_listing.html"
+    change_list_template = "admin/change_list_filter_sidebar.html"
 
     def core(self, obj):
         return obj.framework.core
@@ -43,13 +85,20 @@ class CoreDescAdmin(admin.ModelAdmin):
     list_filter = ('stage', 'core',)
     search_fields = ['descriptor']
     list_per_page = 25
+    change_list_filter_template = "admin/filter_listing.html"
+    change_list_template = "admin/change_list_filter_sidebar.html"
 
 admin.site.register(CoreDesc, CoreDescAdmin)
 
 class FrameworkDescAdmin(admin.ModelAdmin):
     list_display = ['descriptor', 'stage', 'framework']
-    list_filter = ('stage', 'framework',)
+    list_filter = ('framework__core', FrameworkListFilter, 'stage',)
     search_fields = ['descriptor']
     list_per_page = 25
+    change_list_filter_template = "admin/filter_listing.html"
+    change_list_template = "admin/change_list_filter_sidebar.html"
+
+    def core(self, obj):
+        return obj.framework.core
 
 admin.site.register(FrameworkDesc, FrameworkDescAdmin)
--- pts/settings.py
+++ pts/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+GRAPPELLI_ADMIN_TITLE = "PTS 2.0"
+
+#GRAPPELLI_AUTOCOMPLETE_LIMIT = 5
